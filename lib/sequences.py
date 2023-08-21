9954e9ae0b90325d0132559a6ff73176b0475fab
Maxwell <maxwell.nganga@student.moringaschool.com>
2023-08-22 00:59:52 +0300

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
 
-def print_fibonacci(length):
-    pass
+def print_fibonacci(n):
+    fib = [0,1]
+    for index in range(n):
+        add = fib[-1] + fib[-2]
+        fib.append(add)
+        print(fib[0:n])
+
+print_fibonacci(7)
